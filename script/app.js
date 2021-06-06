05a13f64cad5299005235c71f28a69738d21153e
asyncfinkd <shamgoingzuck@gmail.com>
2021-06-06 16:36:44 +0400

    :rocket:

--- script/app.js
+++ script/app.js
@@ -1,10 +1,38 @@
 let ScoreElement = document.querySelector(".game__score");
+let StageElement = document.querySelector(".game__stage");
+let TimerElement = document.querySelector(".timer");
 let Score = 0;
 let Stage = 0;
 let Timer = 50;
+let Interval = 1000;
+let Elements = [
+  {
+    element: ScoreElement,
+    text: "Score ",
+    variable: Score,
+  },
+  {
+    element: StageElement,
+    text: "Stage ",
+    variable: Stage,
+  },
+];
 
 Application();
+StartTimer();
 
 function Application() {
-  ScoreElement.innerHTML = Score;
+  Elements.map((item) => {
+    const { element, text, variable } = item;
+    element.innerHTML = text + variable;
+  });
+}
+
+function timerSetting() {
+  Timer--;
+  TimerElement.innerHTML = Timer + "s";
+}
+
+function StartTimer() {
+  setInterval(timerSetting, Interval);
 }
